2c23b9318d85682afbe1b3ae245881d29272b6c9
dani3lz <danu10024@gmail.com>
2022-10-12 16:36:45 +0300

    fix mock tests

--- src/test/java/com/endava/internship/mocking/repository/InMemUserRepositoryTest.java
+++ src/test/java/com/endava/internship/mocking/repository/InMemUserRepositoryTest.java
@@ -2,21 +2,27 @@ package com.endava.internship.mocking.repository;
 
 import com.endava.internship.mocking.model.Status;
 import com.endava.internship.mocking.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class InMemUserRepositoryTest {
-    private final InMemUserRepository inMemUserRepository = new InMemUserRepository();
+    private InMemUserRepository inMemUserRepository;
+
+    @BeforeEach
+    void setUp() {
+        inMemUserRepository = new InMemUserRepository();
+    }
 
     @Test
     void findByIdWithValidId_shouldReturnUserWithThisId() {
         Optional<User> user = inMemUserRepository.findById(3);
 
-        assertEquals(user, Optional.of(new User(3, "Peter", Status.INACTIVE)));
+        assertThat(user).isEqualTo(Optional.of(new User(3, "Peter", Status.INACTIVE)));
     }
 
     @Test
